9f7a0e11b8a806628cab4aad6e63ad1b837e6e24
Stephan <sgriesel@gmail.com>
2020-07-31 06:48:22 +0200

    add checkout backend

--- server/lib/api.js
+++ server/lib/api.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
 exports.app = express_1.default();
+const checkout_1 = require("./checkout");
 // ALLOW CROSS URL'S TO ACCESS
 exports.app.use(express_1.default.json());
 const cors_1 = __importDefault(require("cors"));
@@ -14,4 +15,14 @@ exports.app.post('/test', (req, res) => {
     const amount = req.body.amount;
     res.status(200).send({ with_tax: amount * 2 }); //
 });
+// CHECKOUT
+exports.app.post('/checkouts/', runAsync(async ({ body }, res) => {
+    res.send(await checkout_1.createStripeCheckoutSession(body.line_items));
+}));
+// CATCH ERRORS
+function runAsync(callback) {
+    return (req, res, next) => {
+        callback(req, res, next).catch(next);
+    };
+}
 //# sourceMappingURL=api.js.map
--- server/src/api.ts
+++ server/src/api.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 export const app = express();
+import { createStripeCheckoutSession } from './checkout';
 
 // ALLOW CROSS URL'S TO ACCESS
 app.use(express.json());
@@ -10,3 +11,18 @@ app.post('/test', (req: Request, res: Response) => {
   const amount = req.body.amount;
   res.status(200).send({ with_tax: amount * 2 }); //
 });
+
+// CHECKOUT
+app.post(
+  '/checkouts/',
+  runAsync(async ({ body }: Request, res: Response) => {
+    res.send(await createStripeCheckoutSession(body.line_items));
+  })
+);
+
+// CATCH ERRORS
+function runAsync(callback: Function) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    callback(req, res, next).catch(next);
+  };
+}
