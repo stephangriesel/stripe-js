765db548780b0999a11f2ae2faa81d1e4404f227
Stephan <sgriesel@gmail.com>
2020-08-05 06:46:17 +0200

    prepare for auth (wip)

--- server/src/api.ts
+++ server/src/api.ts
@@ -1,7 +1,9 @@
 import express from 'express';
-export const app = express();
 import { createStripeCheckoutSession } from './checkout';
 import { createPaymentIntent } from './payments';
+import { auth } from './firebase';
+
+export const app = express();
 
 // ALLOW CROSS URL'S TO ACCESS
 app.use(express.json());
@@ -44,3 +46,58 @@ app.post(
     );
   })
 );
+
+// CUSTOMERS & SETUP INTENTS
+// SAVE CARD ON CUSTOMER RECORD WITH SETUPINTENT
+app.post(
+  '/wallet',
+  runAsync(async (req: Request, res: Response) => {
+    const user = validateUser(req);
+    const setupIntent = await createSetupIntent(user.uid);
+    res.send(setupIntent);
+  })
+);
+
+// RETRIEVE ALL CUSTOMER CARDS
+// app.get(
+//   '/wallet',
+//   runAsync(async (req: Request, res: Response) => {
+//     const user = validateUser(req);
+
+//     const wallet = await listPaymentMethods(user.uid);
+//     res.send(wallet.data);
+//   })
+// );
+
+
+// DECODE FIREBASE TOKEN
+app.use(decodeJWT);
+
+// DECODE TOKEN & MAKE DATA AVAILABLE IN BODY
+async function decodeJWT(req: Request, res: Response, next: NextFunction) {
+  if (req.headers?.authorization?.startsWith('Bearer ')) {
+    const idToken = req.headers.authorization.split('Bearer ')[1];
+
+    try {
+      const decodedToken = await auth.verifyIdToken(idToken);
+      req['currentUser'] = decodedToken;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  next();
+}
+
+
+// ERROR IF USER DOES NOT EXIST
+function validateUser(req: Request) {
+  const user = req['currentUser'];
+  if (!user) {
+    throw new Error(
+      'You must be logged in to make this request. i.e Authroization: Bearer <token>'
+    );
+  }
+
+  return user;
+}
