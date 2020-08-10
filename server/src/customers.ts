6b36ee409ee35684fcfbb3b087b2d8dc24d764fa
Stephan <sgriesel@gmail.com>
2020-08-10 07:21:55 +0200

    setup intent save card for later use

--- server/src/customers.ts
+++ server/src/customers.ts
@@ -2,6 +2,17 @@ import { stripe } from './';
 import { db } from './firebase';
 import { Stripe } from 'stripe';
 
+// SETUPINTENT SAVE CARD FOR LATER USE
+export async function createSetupIntent(userId: string) {
+
+    const customer = await getOrCreateCustomer(userId);
+
+    return stripe.setupIntents.create({
+        customer: customer.id,
+    })
+}
+
+
 //GETS CUSTOMER & CREATE NEW REECORD
 export async function getOrCreateCustomer(userId: string, params?: Stripe.CustomerCreateParams) {
 
